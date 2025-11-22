d27cd2d251843583876469de5be916e2023bbcbc
Vũ Minh Trung <167429293+trung1609@users.noreply.github.com>
2025-11-22 09:11:13 +0700

    Add files via upload

---
+++ luyentap2.sql
@@ -0,0 +1,38 @@
+create table luyentap2.products
+(
+    product_id serial primary key,
+    name       varchar(50),
+    stock      int
+);
+create table luyentap2.sales
+(
+    sale_id    serial primary key,
+    product_id int references luyentap2.products (product_id),
+    quantity   int
+);
+
+create or replace function luyentap2.before_insert_products()
+    returns trigger
+as
+$$
+declare
+    p_stock      int;
+begin
+    select stock into p_stock from luyentap2.products where product_id = new.product_id;
+    if new.quantity > p_stock then
+        raise exception 'Hang trong kho khong du';
+    end if;
+
+    update luyentap2.products set stock = p_stock - new.quantity where products.product_id = new.product_id;
+    return new;
+end;
+$$ language plpgsql;
+
+create trigger trg_before_insert_products
+    before insert
+    on luyentap2.sales
+    for each row
+execute function luyentap2.before_insert_products();
+
+insert into luyentap2.sales (product_id, quantity)
+values (1, 10);
